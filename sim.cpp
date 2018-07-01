884134a7f880972094a82815965a4e79e481b668
Matthew Camarena <camarena.matthew@csu.fullerton.edu>
2018-06-30 19:39:28 -0700

    Added memory class and handlers

--- sim.cpp
+++ sim.cpp
@@ -22,6 +22,118 @@ void showQ(queue <int> tempQ)
 
 }
 
+class MemoryClass
+{
+	public:
+
+	MemoryClass()
+	{
+
+	}
+
+	
+	~MemoryClass()
+	{
+		delete[] mainMemoryPro;
+		delete[] mainMemoryPge;
+	}
+
+	void addSizes(int memSize, int pgeSize)
+	{
+		memorySize = memSize;
+		pageSize = pgeSize;
+		pageTotal = memSize/pgeSize;
+		mainMemoryPro = new int[pageTotal];
+		mainMemoryPge = new int[pageTotal];
+		currMemSize = memSize;
+		for(int i = 0; i < pageTotal; i++)
+		{
+			mainMemoryPro[i] = 0;
+			mainMemoryPge[i] = 0;
+		}
+	}
+
+	bool checkFit(int neededSize)
+	{
+		if (currMemSize >= neededSize)
+			return true;
+		else
+			return false;
+	}
+
+	void insertMem(int memSize, int id)
+	{
+		int proMemory = memSize;
+		int pgeCounter = 1;
+		
+		for(int i = 0; i < pageTotal; i++)
+		{
+			if(proMemory > 0)
+			{
+				if(mainMemoryPro[i] == 0)
+				{
+					mainMemoryPro[i] = id;
+					mainMemoryPge[i] = pgeCounter;
+					pgeCounter++;
+					proMemory -= pageSize;
+					currMemSize -= pageSize;
+
+				}
+			}
+			else 
+			{
+				break;
+			}
+		}
+	}
+
+	void printMemoryMap()
+	{
+		int tempIndex = 0;
+
+		if (currMemSize == memorySize)
+		{
+			cout << "0-" << (memorySize-1) << ":  Free frame(s)" << endl;
+		}
+		else
+		{
+
+		for(int i = 0; i < pageTotal; i++)
+		{
+			if(mainMemoryPro[i] == 0)
+			{
+				tempIndex = i;
+				do
+				{
+					i++;
+				}
+				while(mainMemoryPro[i+1] == 0);
+					cout << "	" << (tempIndex * pageSize) <<  "-" << (i*pageSize) << ": Free frame(s)" << endl;
+			}
+			else 
+			{
+				cout << "	" << (i* pageSize) << "-" << ((i* pageSize) + pageSize - 1);
+				cout << ": Process" << mainMemoryPro[i] << ", " << "Page " << mainMemoryPge[i] << endl;
+
+			}
+		}
+
+
+		}
+	}
+
+
+	int memorySize;
+	int pageSize;
+	int pageTotal;
+	int currMemSize;
+	int *mainMemoryPro;
+	int *mainMemoryPge;
+
+};
+
+
+
 int main()
 {
     int memorySize = 0, pageSize = 0;
@@ -30,6 +142,7 @@ int main()
     int currTime = 0;
 
     queue <int> processQ;
+    MemoryClass mainMemory;
 
     string workloadFilename;
 
@@ -47,7 +160,7 @@ int main()
     else
         pageSize = 300;
 
-
+    mainMemory.addSizes(memorySize, pageSize);
 /*******************************************************************************************
 Read in all the data from the text file and then store it all into a struct
 
@@ -75,6 +188,7 @@ Read in all the data from the text file and then store it all into a struct
     		}
     	}
     	
+
     	/*******************************************************************************************
 		Memory Queue & Manager 
 		Will do the manage the memory of the processes until completed or time reaches 100,000
@@ -87,6 +201,10 @@ Read in all the data from the text file and then store it all into a struct
 	
 	if (currTime == process[processIndex].arrivTime)
 	{
+		/*******************************************************************************************
+		Queue
+
+		********************************************************************************************/
 		cout << "t = " << currTime << ": " <<  "Process " << (processIndex+1) << " arrives" << endl;
 		//TODO: insert into queue
 		processQ.push(processIndex+1);
@@ -98,8 +216,11 @@ Read in all the data from the text file and then store it all into a struct
 		{
 			if (currTime == process[processIndex].arrivTime)
 			{
+				processQ.push(processIndex+1);
 				cout << "       " <<  "Process " << (processIndex+1) << " arrives" << endl;
-				cout << "       " << "Input Queue: " << "[]" << endl;
+				cout << "       " << "Input Queue: " << "[";
+				showQ(processQ);
+				cout << "]" << endl;
 				processIndex++;
 			}
 			else 
@@ -110,6 +231,26 @@ Read in all the data from the text file and then store it all into a struct
 		}
 		tempBool = true;
 	}
+
+	    /*******************************************************************************************
+		Memory Manager
+		
+		********************************************************************************************/
+
+	if(!processQ.empty())
+	{
+
+		//Check if room for first process in Q
+		int proSize = process[processQ.front()].totalMem;
+		if(mainMemory.checkFit(proSize))
+		{
+
+			mainMemory.insertMem(proSize, processQ.front());
+			mainMemory.printMemoryMap();
+		}
+
+	}
+
 
 		currTime++;
 	}
